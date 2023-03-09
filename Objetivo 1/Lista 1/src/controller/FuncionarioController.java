e165f4dda5be47db94e61ad2804547a223858455
Argel <argelhr@hotmail.com>
2023-03-08 22:43:32 -0300

    lista 1

---
+++ Objetivo 1/Lista 1/src/controller/FuncionarioController.java
@@ -0,0 +1,31 @@
+package controller;
+
+import model.Funcionario;
+
+public class FuncionarioController {
+    public static void main(String[] args){
+        Funcionario f1 = new Funcionario();
+        Funcionario f2 = new Funcionario("Puxa saco",5600.99);
+
+        System.out.println("Funcionario 1 com tostring");
+        System.out.println(f1.toString());
+        System.out.println();
+
+        System.out.println("Funcionario 2 com tostring");
+        System.out.println(f2.toString());
+
+        f1.setNome("Jorge");
+        f1.setSalario(2000);
+
+        System.out.println();
+
+        System.out.println("Funcionario 1 com get depois de atualizado");
+        System.out.println(f1.getNome());
+        System.out.println(f1.getSalario());
+        System.out.println();
+
+        System.out.println("Funcionario 2 com get");
+        System.out.println(f2.getNome());
+        System.out.println(f2.getSalario());
+    }
+}
